48df8e2dc884c1cd9abc262984c1411dc3b41a33
Evandabest <evanhaque1@gmail.com>
2024-10-03 02:17:59 -0400

    updated project 2

--- server/src/db.ts
+++ server/src/db.ts
@@ -5,31 +5,31 @@ dotenv.config()
 
 const pg = require('pg')
 
-//const config = {
-//    //user: process.env.PGUSER,
-//    //password: process.env.PGPASSWORD,
-//    //host: process.env.PGHOST,
-//    //port: process.env.PGPORT,
-//    //database: process.env.PGDATABASE
-//    connectionString: process.env.PGCONNECTIONSTRING
-//}
-//
-//
-//const pool = new pg.Pool(config)
-//module.exports = { pool }
-//
-//export async function getAllPokemon(): Promise<pokemon[]> {
-//    const client = await pool.connect();
-//    try {
-//        const res = await client.query('SELECT * FROM pokemon');
-//        console.log(res)
-//        return res.rows;
-//    } finally {
-//        client.release();
-//    }
-//}
-//
-//module.exports = { pool, getAllPokemon };
+const config = {
+    //user: process.env.PGUSER,
+    //password: process.env.PGPASSWORD,
+    //host: process.env.PGHOST,
+    //port: process.env.PGPORT,
+    //database: process.env.PGDATABASE
+    connectionString: process.env.PGCONNECTIONSTRING
+}
+
+
+const pool = new pg.Pool(config)
+module.exports = { pool }
+
+export async function getAllPokemon(): Promise<pokemon[]> {
+    const client = await pool.connect();
+    try {
+        const res = await client.query('SELECT * FROM pokemon');
+        console.log(res)
+        return res.rows;
+    } finally {
+        client.release();
+    }
+}
+
+module.exports = { pool, getAllPokemon };
 
 
 interface pokemon {
@@ -39,42 +39,42 @@ interface pokemon {
     link: string
 }
 
-export const characters: pokemon[] = [
-    {
-        name: "Pikachu",
-        type: "Electric",
-        description: "Pikachu is an Electric type Pokémon introduced in Generation 1. It is known as the Mouse Pokémon.",
-        link: "https://archives.bulbagarden.net/media/upload/thumb/4/4a/0025Pikachu.png/500px-0025Pikachu.png"
-    },
-    {
-        name: "Charmander",
-        type: "Fire",
-        description: "Charmander is a Fire type Pokémon introduced in Generation 1. It is known as the Lizard Pokémon.",
-        link: "https://archives.bulbagarden.net/media/upload/thumb/2/27/0004Charmander.png/500px-0004Charmander.png"
-    },
-    {
-        name: "Squirtle",
-        type: "Water",
-        description: "Squirtle is a Water type Pokémon introduced in Generation 1. It is known as the Tiny Turtle Pokémon.",
-        link: "https://archives.bulbagarden.net/media/upload/thumb/5/54/0007Squirtle.png/500px-0007Squirtle.png"
-    },
-    {
-        name: "Bulbasaur",
-        type: "Grass",
-        description: "Bulbasaur is a Grass type Pokémon introduced in Generation 1. It is known as the Seed Pokémon.",
-        link: "https://archives.bulbagarden.net/media/upload/thumb/f/fb/0001Bulbasaur.png/500px-0001Bulbasaur.png"
-    },
-    {
-        name: "Jigglypuff",
-        type: "Normal",
-        description: "Jigglypuff is a Normal type Pokémon introduced in Generation 1. It is known as the Balloon Pokémon.",
-        link: "https://archives.bulbagarden.net/media/upload/thumb/3/3a/0039Jigglypuff.png/500px-0039Jigglypuff.png"
-    },
-    {
-        name: "Meowth",
-        type: "Normal",
-        description: "Meowth is a Normal type Pokémon introduced in Generation 1. It is known as the Scratch Cat Pokémon.",
-        link: "https://archives.bulbagarden.net/media/upload/thumb/d/d6/0052Meowth.png/500px-0052Meowth.png"
-    }
-    
-]
+//export const characters: pokemon[] = [
+//    {
+//        name: "Pikachu",
+//        type: "Electric",
+//        description: "Pikachu is an Electric type Pokémon introduced in Generation 1. It is known as the Mouse Pokémon.",
+//        link: "https://archives.bulbagarden.net/media/upload/thumb/4/4a/0025Pikachu.png/500px-0025Pikachu.png"
+//    },
+//    {
+//        name: "Charmander",
+//        type: "Fire",
+//        description: "Charmander is a Fire type Pokémon introduced in Generation 1. It is known as the Lizard Pokémon.",
+//        link: "https://archives.bulbagarden.net/media/upload/thumb/2/27/0004Charmander.png/500px-0004Charmander.png"
+//    },
+//    {
+//        name: "Squirtle",
+//        type: "Water",
+//        description: "Squirtle is a Water type Pokémon introduced in Generation 1. It is known as the Tiny Turtle Pokémon.",
+//        link: "https://archives.bulbagarden.net/media/upload/thumb/5/54/0007Squirtle.png/500px-0007Squirtle.png"
+//    },
+//    {
+//        name: "Bulbasaur",
+//        type: "Grass",
+//        description: "Bulbasaur is a Grass type Pokémon introduced in Generation 1. It is known as the Seed Pokémon.",
+//        link: "https://archives.bulbagarden.net/media/upload/thumb/f/fb/0001Bulbasaur.png/500px-0001Bulbasaur.png"
+//    },
+//    {
+//        name: "Jigglypuff",
+//        type: "Normal",
+//        description: "Jigglypuff is a Normal type Pokémon introduced in Generation 1. It is known as the Balloon Pokémon.",
+//        link: "https://archives.bulbagarden.net/media/upload/thumb/3/3a/0039Jigglypuff.png/500px-0039Jigglypuff.png"
+//    },
+//    {
+//        name: "Meowth",
+//        type: "Normal",
+//        description: "Meowth is a Normal type Pokémon introduced in Generation 1. It is known as the Scratch Cat Pokémon.",
+//        link: "https://archives.bulbagarden.net/media/upload/thumb/d/d6/0052Meowth.png/500px-0052Meowth.png"
+//    }
+//    
+//]
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import { characters } from './db';
+import { getAllPokemon } from './db';
 
 //import { characters } from './db';
 const app = express();
@@ -8,38 +8,38 @@ const port = 3000;
 
 app.use(cors()); // Enable CORS for all routes
 
-//app.get('/characters', async (req, res) => {
-//  try {
-//      const pokemon = await getAllPokemon();
-//      res.json(pokemon);
-//  } catch (error) {
-//      console.error('Error fetching Pokémon:', error);
-//      res.status(500).send('Internal Server Error');
-//  }
-//});
-//
-//app.get('/characters/:character', async (req, res) => {
-//  try {
-//      const pokemon = await getAllPokemon();
-//
-//      const character = pokemon.find((character) => character.name === req.params.character);
-//      res.json(character);
-//
-//  } catch (error) {
-//      console.error('Error fetching Pokémon:', error);
-//      res.status(500).send('Internal Server Error');
-//  }
-//});
-
-app.get('/characters', (req, res) => {
-  res.json(characters);
+app.get('/characters', async (req, res) => {
+  try {
+      const pokemon = await getAllPokemon();
+      res.json(pokemon);
+  } catch (error) {
+      console.error('Error fetching Pokémon:', error);
+      res.status(500).send('Internal Server Error');
+  }
 });
 
-app.get('/characters/:character', (req, res) => {
-  const character = characters.find((character) => character.name === req.params.character);
-  res.json(character);
-})
+app.get('/characters/:character', async (req, res) => {
+  try {
+      const pokemon = await getAllPokemon();
 
+      const character = pokemon.find((character) => character.name === req.params.character);
+      res.json(character);
+
+  } catch (error) {
+      console.error('Error fetching Pokémon:', error);
+      res.status(500).send('Internal Server Error');
+  }
+});
+
+//app.get('/characters', (req, res) => {
+//  res.json(characters);
+//});
+//
+//app.get('/characters/:character', (req, res) => {
+//  const character = characters.find((character) => character.name === req.params.character);
+//  res.json(character);
+//})
+//
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
